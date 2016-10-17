6a2058d9d1bd7e104beaf377f32846fd267e5721
Isaac Mac <isaacmac@iMac.local>
2016-10-17 13:51:20 +0200

    Initial Commit

---
+++ TableView y Tab Bar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableView y Tab Bar
+//
+//  Created by Isaac Mac on 17/10/16.
+//  Copyright (c) 2016 Isaac Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TableView y Tab BarTests/TableView_y_Tab_BarTests.swift
@@ -0,0 +1,36 @@
+//
+//  TableView_y_Tab_BarTests.swift
+//  TableView y Tab BarTests
+//
+//  Created by Isaac Mac on 17/10/16.
+//  Copyright (c) 2016 Isaac Mac. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TableView_y_Tab_BarTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
